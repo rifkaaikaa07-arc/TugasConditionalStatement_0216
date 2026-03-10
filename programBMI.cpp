42fea82edc1aae21773be32230b73f906aa622c2
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-10 09:18:35 +0700

    menambahkan input

--- programBMI.cpp
+++ programBMI.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 float tinggi, Berat, hasilBMI;
 
+void input(){
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+    cout << "Masukkan berat badan (kg): ";
+    cin >> Berat;
+}
 int main (){
     return 0;
 }
