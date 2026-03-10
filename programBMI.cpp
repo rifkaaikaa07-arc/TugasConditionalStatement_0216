04000b13a05dfcc6e778f48005b62c09127c0659
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-10 09:19:58 +0700

    menambahkan kondisi bmi

--- programBMI.cpp
+++ programBMI.cpp
@@ -13,6 +13,17 @@ void input(){
 float menghitungBMI (float tinggi,float Berat){
     return Berat/(tinggi * tinggi);
 }
+
+string kondisiBMI (float BMI){
+    if (BMI < 18.5)
+       return "berat badan kurang";
+    else if (BMI < 25)
+       return "berat badan normal";
+    else if (BMI < 30)
+       return "berat badan kelebihan";
+    else
+       return "obesitas";
+}
 int main (){
     return 0;
 }
